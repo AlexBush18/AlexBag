0b4410faf3aa63a917e7c347e06455e092bc2ac9
AlexBush18 <ramza1818@gmail.com>
2024-01-02 20:10:00 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const FirstName = 'Александр'
+console.log('Меня зовут', FirstName)
+
+const MyCity = 'Жодино'
+console.log('Мой город', MyCity)
+
+const Age = '40 лет'
+console.log('Мой возраст', Age)
+
+const Hobby = 'Футбол, покер, UMK3'
+console.log('Мое хобби', Hobby)
