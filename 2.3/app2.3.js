fc0b12902274bce2ae5024007b429d53b088d78e
AlexBush18 <ramza1818@gmail.com>
2024-01-04 19:16:33 +0300

    2.3

---
+++ 2.3/app2.3.js
@@ -0,0 +1,18 @@
+const titleProject = 'Оценка стоимости'
+console.log('Название проекта', titleProject)
+
+const screensValue = 'Типы экранов'
+console.log('шаблонные, с уникальным дизайном, с анимациями', screensValue)
+
+const screenPrice = '58'
+console.log('Цена одного экрана', screenPrice)
+
+const percentage = '33'
+console.log('откат подрядчику', percentage)
+
+const fullPrice = '450'
+console.log('сумма заработка', fullPrice)
+
+const responsive = new Boolean(true);
+
+
