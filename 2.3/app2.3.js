7843368226c27e937802a7396f50247a3de6873e
AlexBush18 <ramza1818@gmail.com>
2024-01-05 16:20:29 +0300

    2.3

--- 2.3/app2.3.js
+++ 2.3/app2.3.js
@@ -2,11 +2,11 @@ let titleProject = 'Оценка стоимости'
 
 let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
 
-let screenPrice = '58'
+let screenPrice = 58
 
-let percentage = '33'
+let percentage = 33
 
-let fullPrice = '450'
+let fullPrice = 450
 
 
 let responsive = true;
