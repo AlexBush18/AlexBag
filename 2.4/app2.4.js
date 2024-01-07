c4b9bc6ddd816bff375557a6437c47764a4a58fe
AlexBush18 <ramza1818@gmail.com>
2024-01-07 16:35:57 +0300

    2.4

---
+++ 2.4/app2.4.js
@@ -0,0 +1,46 @@
+// let titleProject = 'Оценка стоимости'
+
+// let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+
+// let screenPrice = 58
+
+// let percentage = 33
+
+// let fullPrice = 450
+
+// let responsive = true;
+
+alert('Привет')
+
+let titleProject = prompt('Название проекта?');
+
+let screensValue = prompt('шаблонные, с уникальным дизайном, с анимациями');
+
+
+let responsive = confirm("Нужен ли респонсивный сайт?");
+
+let service1 = prompt ('Какой сервис нужен ?')
+let servicePrice1 = parseFloat(prompt("Сколько это будет стоить?"));
+
+let service2 = prompt ('Какой еще сервис тебе нужен?');
+let servicePrice2 = parseFloat(prompt("Сколько будет стоить этот второй сервис?"));
+
+let screenPrice = 458;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+
+let partnerCommission = 0.13;
+let servicePercentPrice = Math.round(fullPrice * partnerCommission);
+
+console.log('Тоговая стоимость проекта после комиссии') + servicePercentPrice;
+
+
+if (fullPrice > 50000) {
+    console.log('Сделаем скидку 10%');
+} else if (fullPrice > 20000 && fullPrice < 40000) {
+    console.log("Сделаем скидку 5%");
+  } else if (fullPrice < 20000 && fullPrice > 0) {
+    console.log("Скидка не предусмотрена");
+  } else if (fullPrice <= 0) {
+    console.log("Что-то пошло не так");
+}
